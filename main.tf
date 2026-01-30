48dfaaa7e57ba1c1825f95f715739ba9d082e6d6
Ankit Khokhar <ankitkhokhar@Ankits-MacBook-Pro.local>
2026-01-30 22:20:33 +0530

    terraform apply test

--- main.tf
+++ main.tf
@@ -9,32 +9,32 @@ module "vpc" {
   environment        = var.environment
 }
 
-# module "eks" {
-#   source = "./modules/eks"
+module "eks" {
+  source = "./modules/eks"
 
-#   cluster_name        = var.cluster_name
-#   cluster_version     = var.cluster_version
-#   environment         = var.environment
-#   vpc_id              = module.vpc.vpc_id
-#   private_subnet_ids  = module.vpc.private_subnet_ids
-#   admin_principal_arn = var.admin_principal_arn
-#   account_id = var.account_id
-# }
+  cluster_name        = var.cluster_name
+  cluster_version     = var.cluster_version
+  environment         = var.environment
+  vpc_id              = module.vpc.vpc_id
+  private_subnet_ids  = module.vpc.private_subnet_ids
+  admin_principal_arn = var.admin_principal_arn
+  account_id = var.account_id
+}
 
-# module "default_node_group" {
-#   source = "./modules/node-group"
+module "default_node_group" {
+  source = "./modules/node-group"
 
-#   cluster_name = module.eks.cluster_name
-#   node_group_name = "default"
-#   subnet_ids = module.vpc.private_subnet_ids
-#   instance_types = var.node_instance_types
-#   desired_size = 1
-#   min_size = 1
-#   max_size = 2
-#   environment = var.environment
+  cluster_name = module.eks.cluster_name
+  node_group_name = "default"
+  subnet_ids = module.vpc.private_subnet_ids
+  instance_types = var.node_instance_types
+  desired_size = 1
+  min_size = 1
+  max_size = 2
+  environment = var.environment
 
-#   labels = {
-#     role = "general"
-#   }
+  labels = {
+    role = "general"
+  }
 
-# }
+}
