256811da9cf1bc2e36936024317b0bf50485e366
Ankit Khokhar <ankitkhokhar@Ankits-MacBook-Pro.local>
2026-02-02 21:25:39 +0530

    multi -tenant support terraform

--- outputs.tf
+++ outputs.tf
@@ -2,22 +2,47 @@ output "vpc_id" {
     value = module.vpc.vpc_id
 }
 
-output "private_subnet_ids" {
-    value = module.vpc.private_subnet_ids
+output "vpc_ids" {
+  value = {
+    for k, v in module.vpc:
+    k => v.vpc_id
+  }
 }
+
+output "private_subnet_ids" {
+  value = {
+    for k, v in module.vpc :
+    k => v.private_subnet_ids
+  }
+}
+
 
 output "public_subnet_ids" {
-    value = module.vpc.public_subnet_ids
+    value = {
+      for k, v in module.vpc :
+      k => v.public_subnet_ids
+    }
 }
 
-output "eks_cluster_name" {
-  value = module.eks.cluster_name
+output "eks_cluster_names" {
+  value = {
+    for k, v in module.eks :
+    k => v.cluster_name
+  }
 }
 
-output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+
+output "eks_cluster_endpoints" {
+  value = {
+    for k, v in module.eks :
+    k => v.cluster_endpoint
+  }
 }
 
-output "eks_oidc_provider_arn" {
-  value = module.eks.oidc_provider_arn
+
+output "eks_oidc_provider_arns" {
+  value = {
+    for k, v in module.eks :
+    k => v.oidc_provider_arn
+  }
 }
